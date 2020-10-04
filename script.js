4fd42e3274f56ecf7cc35e9adfd3de0109413144
Titus Vimal Raj <titusvimalraj@gmail.com>
2020-10-04 09:22:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -104,46 +104,134 @@ let generateChartForDashboard = (chartData) => {
   });
 
   console.log(lineGraphChartData);
-
-  let chart = new Chart(ctx, {
-    type: 'line',
-    data: lineGraphChartData,
+  let chartCfg = {
+    data: {
+      datasets: [{
+        label: 'CHRT - Chart.js Corporation',
+        backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.red,
+        data: lineGraphChartData,
+        type: 'line',
+        pointRadius: 0,
+        fill: false,
+        lineTension: 0,
+        borderWidth: 2
+      }]
+    },
     options: {
+      animation: {
+        duration: 0
+      },
       scales: {
         xAxes: [{
           type: 'time',
-          time: {
-            displayFormats: {
-              'millisecond': 'MMM DD',
-              'second': 'MMM DD',
-              'minute': 'MMM DD',
-              'hour': 'MMM DD',
-              'day': 'MMM DD',
-              'week': 'MMM DD',
-              'month': 'MMM DD',
-              'quarter': 'MMM DD',
-              'year': 'MMM DD',
-            },
-            unit: 'month',
-            tooltipFormat: 'll HH:mm',
-            unitStepSize: 1,
-            minUnit: 'day'
-          },
+          distribution: 'series',
+          offset: true,
           ticks: {
-            beginAtZero: true,
-            stepSize: 1
+            major: {
+              enabled: true,
+              fontStyle: 'bold'
+            },
+            source: 'data',
+            autoSkip: true,
+            autoSkipPadding: 75,
+            maxRotation: 0,
+            sampleSize: 100
+          },
+          afterBuildTicks: function (scale, ticks) {
+            var majorUnit = scale._majorUnit;
+            var firstTick = ticks[0];
+            var i, ilen, val, tick, currMajor, lastMajor;
+
+            val = moment(ticks[0].value);
+            if ((majorUnit === 'minute' && val.second() === 0)
+              || (majorUnit === 'hour' && val.minute() === 0)
+              || (majorUnit === 'day' && val.hour() === 9)
+              || (majorUnit === 'month' && val.date() <= 3 && val.isoWeekday() === 1)
+              || (majorUnit === 'year' && val.month() === 0)) {
+              firstTick.major = true;
+            } else {
+              firstTick.major = false;
+            }
+            lastMajor = val.get(majorUnit);
+
+            for (i = 1, ilen = ticks.length; i < ilen; i++) {
+              tick = ticks[i];
+              val = moment(tick.value);
+              currMajor = val.get(majorUnit);
+              tick.major = currMajor !== lastMajor;
+              lastMajor = currMajor;
+            }
+            return ticks;
           }
         }],
         yAxes: [{
-          ticks: {
-            max: 25,
-            min: 0,
-            stepSize: 1
+          gridLines: {
+            drawBorder: false
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of URLs Per day'
           }
         }]
+      },
+      tooltips: {
+        intersect: false,
+        mode: 'index',
+        callbacks: {
+          label: function (tooltipItem, myData) {
+            var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+            if (label) {
+              label += ': ';
+            }
+            label += parseFloat(tooltipItem.value).toFixed(2);
+            return label;
+          }
+        }
       }
     }
-  });
+  }
+  let chart = new Chart(ctx, chartCfg);
+
+  // let chart = new Chart(ctx, {
+  //   type: 'line',
+  //   data: lineGraphChartData,
+  //   options: {
+  //     scales: {
+  //       xAxes: [{
+  //         type: 'time',
+  //         time: {
+  //           displayFormats: {
+  //             'millisecond': 'MMM DD',
+  //             'second': 'MMM DD',
+  //             'minute': 'MMM DD',
+  //             'hour': 'MMM DD',
+  //             'day': 'MMM DD',
+  //             'week': 'MMM DD',
+  //             'month': 'MMM DD',
+  //             'quarter': 'MMM DD',
+  //             'year': 'MMM DD',
+  //           },
+  //           unit: 'month',
+  //           tooltipFormat: 'll HH:mm',
+  //           unitStepSize: 1,
+  //           minUnit: 'day'
+  //         },
+  //         ticks: {
+  //           beginAtZero: true,
+  //           stepSize: 1
+  //         }
+  //       }],
+  //       yAxes: [{
+  //         ticks: {
+  //           max: 25,
+  //           min: 0,
+  //           stepSize: 1
+  //         }
+  //       }]
+  //     }
+  //   }
+  // });
 
   console.log(chart);
 }
