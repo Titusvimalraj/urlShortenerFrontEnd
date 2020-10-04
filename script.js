519c1feffb1467fdd60403501034f4c9263039d0
Titus Vimal Raj <titusvimalraj@gmail.com>
2020-10-04 09:43:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -104,93 +104,140 @@ let generateChartForDashboard = (chartData) => {
   });
   let color = Chart.helpers.color;
   console.log(lineGraphChartData);
+
+
   let chartCfg = {
+    type: 'line',
     data: {
       datasets: [{
-        label: 'uriShorty',
+        label: 'Urls',
         backgroundColor: "rgb(245, 66, 111)",
         borderColor: "rgb(245, 66, 111)",
-        data: lineGraphChartData,
-        type: 'line',
-        pointRadius: 0,
         fill: false,
-        lineTension: 0,
-        borderWidth: 2
+        data: lineGraphChartData
       }]
     },
     options: {
-      animation: {
-        duration: 0
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Short Urls Created by you at uriShorty'
       },
       scales: {
         xAxes: [{
           type: 'time',
-          distribution: 'series',
-          offset: true,
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          },
           ticks: {
             major: {
-              enabled: true,
-              fontStyle: 'bold'
-            },
-            source: 'data',
-            autoSkip: true,
-            autoSkipPadding: 75,
-            maxRotation: 0,
-            sampleSize: 100
-          },
-          afterBuildTicks: function (scale, ticks) {
-            var majorUnit = scale._majorUnit;
-            var firstTick = ticks[0];
-            var i, ilen, val, tick, currMajor, lastMajor;
-
-            val = moment(ticks[0].value);
-            if ((majorUnit === 'minute' && val.second() === 0)
-              || (majorUnit === 'hour' && val.minute() === 0)
-              || (majorUnit === 'day' && val.hour() === 9)
-              || (majorUnit === 'month' && val.date() <= 3 && val.isoWeekday() === 1)
-              || (majorUnit === 'year' && val.month() === 0)) {
-              firstTick.major = true;
-            } else {
-              firstTick.major = false;
+              fontStyle: 'bold',
+              fontColor: '#FF0000'
             }
-            lastMajor = val.get(majorUnit);
-
-            for (i = 1, ilen = ticks.length; i < ilen; i++) {
-              tick = ticks[i];
-              val = moment(tick.value);
-              currMajor = val.get(majorUnit);
-              tick.major = currMajor !== lastMajor;
-              lastMajor = currMajor;
-            }
-            return ticks;
           }
         }],
         yAxes: [{
-          gridLines: {
-            drawBorder: false
-          },
+          display: true,
           scaleLabel: {
             display: true,
-            labelString: 'Number of URLs Per day'
+            labelString: 'value'
           }
         }]
-      },
-      tooltips: {
-        intersect: false,
-        mode: 'index',
-        callbacks: {
-          label: function (tooltipItem, myData) {
-            var label = myData.datasets[tooltipItem.datasetIndex].label || '';
-            if (label) {
-              label += ': ';
-            }
-            label += parseFloat(tooltipItem.value).toFixed(2);
-            return label;
-          }
-        }
       }
     }
-  }
+  };
+
+
+
+  // let chartCfg = {
+  //   data: {
+  //     datasets: [{
+  //       label: 'uriShorty',
+  //       backgroundColor: "rgb(245, 66, 111)",
+  //       borderColor: "rgb(245, 66, 111)",
+  //       data: lineGraphChartData,
+  //       type: 'line',
+  //       pointRadius: 0,
+  //       fill: false,
+  //       lineTension: 0,
+  //       borderWidth: 2
+  //     }]
+  //   },
+  //   options: {
+  //     animation: {
+  //       duration: 0
+  //     },
+  //     scales: {
+  //       xAxes: [{
+  //         type: 'time',
+  //         distribution: 'series',
+  //         offset: true,
+  //         ticks: {
+  //           major: {
+  //             enabled: true,
+  //             fontStyle: 'bold'
+  //           },
+  //           source: 'data',
+  //           autoSkip: true,
+  //           autoSkipPadding: 75,
+  //           maxRotation: 0,
+  //           sampleSize: 100
+  //         },
+  //         afterBuildTicks: function (scale, ticks) {
+  //           var majorUnit = scale._majorUnit;
+  //           var firstTick = ticks[0];
+  //           var i, ilen, val, tick, currMajor, lastMajor;
+
+  //           val = moment(ticks[0].value);
+  //           if ((majorUnit === 'minute' && val.second() === 0)
+  //             || (majorUnit === 'hour' && val.minute() === 0)
+  //             || (majorUnit === 'day' && val.hour() === 9)
+  //             || (majorUnit === 'month' && val.date() <= 3 && val.isoWeekday() === 1)
+  //             || (majorUnit === 'year' && val.month() === 0)) {
+  //             firstTick.major = true;
+  //           } else {
+  //             firstTick.major = false;
+  //           }
+  //           lastMajor = val.get(majorUnit);
+
+  //           for (i = 1, ilen = ticks.length; i < ilen; i++) {
+  //             tick = ticks[i];
+  //             val = moment(tick.value);
+  //             currMajor = val.get(majorUnit);
+  //             tick.major = currMajor !== lastMajor;
+  //             lastMajor = currMajor;
+  //           }
+  //           return ticks;
+  //         }
+  //       }],
+  //       yAxes: [{
+  //         gridLines: {
+  //           drawBorder: false
+  //         },
+  //         scaleLabel: {
+  //           display: true,
+  //           labelString: 'Number of URLs Per day'
+  //         }
+  //       }]
+  //     },
+  //     tooltips: {
+  //       intersect: false,
+  //       mode: 'index',
+  //       callbacks: {
+  //         label: function (tooltipItem, myData) {
+  //           var label = myData.datasets[tooltipItem.datasetIndex].label || '';
+  //           if (label) {
+  //             label += ': ';
+  //           }
+  //           label += parseFloat(tooltipItem.value).toFixed(2);
+  //           return label;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
   let chart = new Chart(ctx, chartCfg);
 
   // let chart = new Chart(ctx, {
